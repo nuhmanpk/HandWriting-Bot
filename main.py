9e19b79b28ed973e1f416c1a97500b4f982a8577
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-10-10 18:48:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+
+# © BugHunterCodeLabs ™
+# © bughunter0
+# 2021
+# Copyright - https://en.m.wikipedia.org/wiki/Fair_use
+
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import User, Message
+import os
+import requests
+
+bughunter0 = Client(
+    "Handwriting",
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"],
+)
+
+
+@bughunter0.on_message(filters.text)
+async def text(bot, message):
+    text = str(message.text)
+    chat_id = int(message.chat.id)
+    file_name = f"{message.chat.id}.jpg"
+    length = len(text)
+    if length < 500:
+        txt = await message.reply_text("Converting to handwriting...")
+        rgb = [0, 0, 0]
+        try:
+            data = requests.get(
+                "https://pywhatkit.herokuapp.com/handwriting?text=%s&rgb=%s,%s,%s"
+                % (text, rgb[0], rgb[1], rgb[2])
+            ).content
+        except Exception as error:
+            await message.reply_text(f"{error}")
+            return
+        with open(file_name, "wb") as file:
+            file.write(data)
+            file.close()
+        await txt.edit("Uploading...")
+        await bot.send_photo(
+            chat_id=chat_id,
+            photo=file_name,
+            caption="Join @BugHunterBots"
+        )
+        await txt.delete()
+        os.remove(file_name)
+    else:
+        await message.reply_text("Please don't do It")
+
+
+bughunter0.run()
+
